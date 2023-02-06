0ea8d76b15375264792bdaec0594bb4c9daf63df
AbdulSaboor16 <as2536475@gmail.com>
2023-02-06 22:34:15 +0500

    add

--- lib/add_product.dart
+++ lib/add_product.dart
@@ -1,11 +1,25 @@
+import 'package:addprouctfirebase/data.dart';
+import 'package:addprouctfirebase/nextpage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddProductView extends StatelessWidget {
+class AddProductView extends StatefulWidget {
   AddProductView({super.key});
+
+  @override
+  State<AddProductView> createState() => _AddProductViewState();
+}
+
+class _AddProductViewState extends State<AddProductView> {
   TextEditingController productName = TextEditingController();
+
   TextEditingController productPrice = TextEditingController();
+
   TextEditingController productdescription = TextEditingController();
+
+  List number = [
+    {"amount": 12, "isfavorite": false}
+  ];
 
   addProduct() async {
     CollectionReference products =
@@ -20,34 +34,100 @@ class AddProductView extends StatelessWidget {
         .catchError((e) => print(e));
   }
 
+  AddNumber() async {
+    CollectionReference numbe = FirebaseFirestore.instance.collection("numbe");
+    await numbe
+        .add({
+          "amount": number,
+          "isfavorite": number,
+        })
+        .then((value) => print("isfavorite Addes"))
+        .catchError((e) => print(e));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
       appBar: AppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              controller: productName,
+      body: ListView.builder(
+        itemCount: number.length,
+        itemBuilder: (context, index) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  controller: productName,
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  controller: productPrice,
+                ),
+                TextField(
+                  controller: productdescription,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    addProduct();
+                  },
+                  child: const Text("Add Product"),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 350,
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 50,
+                            width: 100,
+                            alignment: Alignment.center,
+                            child: Text("${number[index]["amount"]}"),
+                          ),
+                          Container(
+                            height: 50,
+                            width: 100,
+                            child: IconButton(
+                                onPressed: () {
+                                  AddNumber();
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const NextPage(),
+                                    ),
+                                  );
+                                  if (number[index]["isfavorite"] == true) {
+                                    setState(() {
+                                      number[index]['isfavorite'] = false;
+                                      
+                                    });
+                                  } else {
+                                    setState(() {
+                                      favoriteItem.add(number[index]);
+                                      number[index]["isfavorite"] = true;
+                                    });
+                                  }
+                                },
+                                icon: Icon(Icons.favorite,
+                                    color: number[index]["isfavorite"]
+                                        ? Colors.red
+                                        : Colors.grey)),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                )
+              ],
             ),
-            TextField(
-              keyboardType: TextInputType.number,
-              controller: productPrice,
-            ),
-            TextField(
-              controller: productdescription,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                addProduct();
-              },
-              child: const Text("Add Product"),
-            )
-          ],
-        ),
+          );
+        },
       ),
     );
   }
---
+++ lib/nextpage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class NextPage extends StatefulWidget {
+  const NextPage({super.key});
+
+  @override
+  State<NextPage> createState() => _NextPageState();
+}
+
+class _NextPageState extends State<NextPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
